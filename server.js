68ccd575fdd6d30831c0ebcdf699634d568d5eab
Sanjeev Kumar <sanjeevscet@gmail.com>
2023-06-04 13:21:03 +0530

    Adding GRPC server

---
+++ server.js
@@ -0,0 +1,77 @@
+import grpc from "@grpc/grpc-js";
+import protoLoader from "@grpc/proto-loader";
+
+const PROTO_PATH = "./news.proto";
+const options = {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+}
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, options);
+const newsProto = grpc.loadPackageDefinition(packageDefinition)
+const server = new grpc.Server();
+
+let news = [
+    { id: "1", title: "Note 12", body: "Content 1", postImage: "Post Image 1" },
+    { id: "2", title: "Note 22", body: "Content 2", postImage: "Post Image 2" }
+];
+
+server.addService(newsProto.NewsService.service, {
+    // get All News
+    getAllNews: (_, callback) => {
+        callback(null, { news });
+    },
+
+    // get News By Id
+    getNews: (data, callback) => {
+        const newsId = data.request.id;
+        const newsItem = news.find(({ id }) => id === newsId);
+        // if data not found, change status code & add error msg
+        if (!newsItem) {
+            const error = {
+                code: grpc.status.NOT_FOUND,
+                message: 'Sanjeev, Data not found!',
+            };
+            callback(error)
+
+        }
+        callback(null, newsItem)
+    },
+
+    // delete News By Id
+    deleteNews: (data, callback) => {
+        const newsId = data.request.id;
+        news = news.filter(({ id }) => id !== newsId)
+
+        callback(null, {})
+    },
+
+    // Update Passed Id's News
+    editNews: (data, callback) => {
+        const { id: newsId, body, title, postImage } = data.request;
+        const newsItem = news.find(({ id }) => id === newsId);
+        Object.assign(newsItem, { body, title, postImage });
+
+        callback(null, newsItem)
+    },
+
+    // Add a new News
+    addNews: (data, callback) => {
+        const payload = { ...data.request };
+        console.log({ payload })
+        let _news = { id: Date.now(), ...payload };
+        news.push(_news);
+        console.log({ news })
+
+        callback(null, _news)
+    },
+});
+
+
+server.bindAsync("127.0.0.1:50051", grpc.ServerCredentials.createInsecure(), (_, port) => {
+    console.log("Server at port:", port)
+    console.log("Server running at http://127.0.0.1:50051");
+    server.start();
+})
